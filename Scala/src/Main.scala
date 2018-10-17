ce408abf0c9ea758f691eb885b8694c0bea90b22
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2018-10-17 15:57:10 +0300

    +++

--- Scala/src/Main.scala
+++ Scala/src/Main.scala
@@ -3,19 +3,19 @@ object Main {
     val AudiA6 = new CarSample("Audi A6", "Diesel", 2.0, 5, 0.2)
     val BMV535 = new CarSample("BMW535", "Diesel", 3.0, 5, 0.3)
     val Tesla = new CarSample("Tesla", "Electricity", 1.0, 2, 0.18)
-    val BMX = new BicycleSample("BMX", "ECO", 1, 0.08)
+    val BMX = new BicycleSample("BMX", "ECO", 1, 0.25)
 
 
     val rentFirstCar = new RentCarInSimpleModeUntilDay(AudiA6, 10, 5)
     rentFirstCar.MakeADeal
 
-    val rentSecondCar = new RentCarInSimpleModeUnilMonth(BMV535, 8 * 1440, 123)
+    val rentSecondCar = new RentCarInSimpleModeUnilMonth(BMV535, 8 * 14, 123)
     rentSecondCar.MakeADeal
 
-    val rentThirdCar = new RentCarInSimpleModeUnilMonth(Tesla, 30 * 1440, 74)
+    val rentThirdCar = new RentCarInSimpleModeUnilMonth(Tesla, 30 * 14, 74)
     rentThirdCar.MakeADeal
 
-    val rentFirstBicycle = new RentBicycleUntilDay(BMX, 30)
+    val rentFirstBicycle = new RentBicycleUntilDay(BMX, 60)
     rentFirstBicycle.MakeADeal
   }
 }
